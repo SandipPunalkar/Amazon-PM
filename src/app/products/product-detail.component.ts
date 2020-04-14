b4685a7a3d97ddd72b9b9eaf91e64c3e72dcbae1
Sandip Punalkar <sandippunalkar9@gmail.com>
2020-04-13 23:58:07 -0700

    Configuring Routes

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+})
+export class ProductDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
